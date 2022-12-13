792de2496918345b51476dd01cbc684d2ae0859d
julio santacruz <aram.jsh1@gmail.com>
2022-12-13 15:03:58 -0800

    hotfix

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -4,21 +4,12 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
-  // window.dataLayer = window.dataLayer || [];
-  // function gtag() {
-  //   dataLayer.push(arguments);
-  // }
-  // gtag("js", new Date());
-
-  // gtag("config", "G-7GKX3LC9WK");
+ 
 
   return (
     <>
       <Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-7GKX3LC9WK"
-        ></script>
+        
       </Head>
       <Navbar />
 
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,20 +8,14 @@ class MyDocument extends Document {
           <title>BIMCapacitacion</title>
           <meta charSet="utf-8" />
            
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-7GKX3LC9WK"
-          ></script>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-7GKX3LC9WK"></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag() {
-              dataLayer.push(arguments);
-            }
-            gtag("js", new Date());
-          
-            gtag("config", "G-7GKX3LC9WK");
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7GKX3LC9WK');
             `,
             }}
           />
